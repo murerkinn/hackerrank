393962d643abbf546f9aa52e772b9e2d5f88b0f1
Murat Erkin Çiçek <57751146+murerkinn@users.noreply.github.com>
2023-01-21 22:07:34 +0300

    Add SQL Weather Observaton challenges from 1 to 5

---
+++ sql/basic-select/weather-observation-station-5.sql
@@ -0,0 +1,22 @@
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name).
+-- If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+-- The STATION table is described as follows:
+
+-- ------------------------------
+-- | Field | Type               |
+-- ------------------------------
+-- | ID    | NUMBER             |
+-- | CITY  | VARCHAR2(21)       |
+-- | STATE | VARCHAR2(2)        |
+-- | LAT_N | NUMBER             |
+-- | LONG_W| NUMBER             |
+-- ------------------------------
+
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+-- When ordered alphabetically, the CITY names are listed as "ABC", "DEF", "PQRS", and "WXY", with lengths 3, 3, 4 and 3.
+-- The longest name is "PQRS", but there are 3 options for shortest named city. Choose "ABC", because it comes first alphabetically.
+
+-- MySQL
+SELECT DISTINCT city, LENGTH(city) FROM station ORDER BY LENGTH(city) ASC, city ASC LIMIT 1;
+SELECT DISTINCT city, LENGTH(city) FROM station ORDER BY LENGTH(city) DESC, city ASC LIMIT 1;
